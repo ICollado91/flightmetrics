fcc1cde279c48fd7b2b32f0d4c6ff193d29ddf55
Ivan <collado.ip@gmail.com>
2017-08-24 12:22:00 +0000

    user styles

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,6 +39,9 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
+  
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
